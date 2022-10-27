a18f85227fbf4acc90a0717859ed6796e80ebc67
Zach Avraham <zach@navina.ai>
2022-10-27 16:39:38 +0300

    Update version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as req:
 
 setuptools.setup(
     name='BERN2',
-    version='0.1.1',
+    version='0.1.2',
     scripts=[],
     author="Navina ai",
     author_email="tech@navina.ai",
