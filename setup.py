4a129ea476e476170cf6f699ee1f2d4dd185cf65
zach-navina <70964159+zach-navina@users.noreply.github.com>
2022-10-27 14:56:47 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt', 'r') as req:
+    packages = req.read().splitlines()
+    try:
+        packages.remove('')
+    except:
+        pass
+
+setuptools.setup(
+    name='BERN2',
+    version='0.1.0',
+    scripts=[],
+    author="Navina ai",
+    author_email="tech@navina.ai",
+    description="BERN2",
+    long_description="BERN2",
+    long_description_content_type="text/markdown",
+    install_requires=packages,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
