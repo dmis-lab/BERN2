406ae55119440f121c041bf93b19750516717059
Zach Avraham <zach@navina.ai>
2022-11-02 17:28:16 +0200

    Fix an error in the model name.

--- temp_name_021122/run_bern2.py
+++ temp_name_021122/run_bern2.py
@@ -333,7 +333,7 @@ class LocalBERN2():
         start_time = time.time()
         argparser = argparse.ArgumentParser()
         argparser.add_argument('--seed', type=int, help='random seed for initialization', default=1)
-        argparser.add_argument('--model_name_or_path', default='dmis-lab/temp_name_021122-ner')
+        argparser.add_argument('--model_name_or_path', default='dmis-lab/bern2-ner')
         argparser.add_argument('--max_seq_length', type=int,
                                help='The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.',
                                default=128)
