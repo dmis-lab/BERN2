cc08980a916e1fde80f1919a3827f9674c1caeca
Zach Avraham <zach@navina.ai>
2022-11-28 16:33:18 +0200

    Update output to be list-like.

--- bern2/run_bern2.py
+++ bern2/run_bern2.py
@@ -383,12 +383,11 @@ def run_bern2_on_batch(df: pd.DataFrame, text_col: str = 'content'):
     return res_df
 
 
-def run_bern2_annotation(list_of_texts: str) -> list:
+def run_bern2_annotation(list_of_texts: list) -> list:
     if initialize_bern2_annotator.annotator is None:
         raise Exception('BERN2 annotator is not initialized!')
     result = initialize_bern2_annotator.annotator.annotate_text(list_of_texts)
-    annotations = result['annotations']
-    return annotations
+    return result
 
 
 def initialize_bern2_annotator(max_word_len: int = 50,
