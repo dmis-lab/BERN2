bda0af5a603f50b0e8c3594bdc277b421c3a0ca0
Zach Avraham <zach@navina.ai>
2023-01-01 11:48:05 +0200

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as req:
 
 setuptools.setup(
     name='BERN2',
-    version='0.3.6',
+    version='0.3.7',
     scripts=[],
     author="Navina ai",
     author_email="tech@navina.ai",
